8844497ef2633f0ef48eab7082696fe27ea09e44
Elkeno Jones <elkeno.jones@binarystar.ca>
2025-03-05 15:00:11 -0400

    feat: update the about page

--- red-canyon-web/src/views/AboutView.vue
+++ red-canyon-web/src/views/AboutView.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About</h1>
+    <p>
+      This web app serves to help you plan your trip to Prince Edward Island.
+
+      Never been? No worries, this app will give you suggestions based on your chosen itinerary type.
+      Give it a try, its free to use.
+    </p>
   </div>
 </template>
 
