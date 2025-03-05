03bf19bbfe62ac224d91677662639bc1beadcdad
Elkeno Jones <elkeno.jones@binarystar.ca>
2025-03-04 23:50:33 -0400

    feat: restart vue application with Vuejs 3

---
+++ red-canyon-web/src/components/ItineraryTypes.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const props = defineProps<{
+  test: string,
+  types: any,
+}>()
+
+console.log(props.types);
+</script>
+<template>
+  <div class="it-card">
+    <h2>Itinerary Type Card {{ test }}</h2>
+    <hr />
+    <table>
+      <thead>
+        <tr><th>ID</th><th>Value</th></tr>
+      </thead>
+      <tr v-for="type in props.types">
+        <td>{{ type.id }}</td><td>{{ type.value }}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<style scoped>
+  .it-card {
+    border: 2px solid red;
+  }
+</style>
---
+++ red-canyon-web/src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+import { defineAsyncComponent } from 'vue'
+import ItineraryTypes from '../components/ItineraryTypes.vue'
+import { ref } from 'vue'
+
+let test = 'gettypes'
+let types = ref([
+  { id: "1", value: "one" },
+  { id: "2", value: "two" },
+  { id: "3", value: "three" }
+])
+</script>
+
+<template>
+  <main>
+    <ItineraryTypes :test="test" :types="types" />
+  </main>
+</template>
