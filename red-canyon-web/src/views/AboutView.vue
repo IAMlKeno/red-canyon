cf1108d79f4ebe485f066cb6766860991da59f89
Elkeno Jones <elkeno.jones@binarystar.ca>
2025-04-06 19:36:23 -0300

    feat: update the about page

--- red-canyon-web/src/views/AboutView.vue
+++ red-canyon-web/src/views/AboutView.vue
@@ -2,9 +2,19 @@
   <div class="about">
     <h1>About</h1>
     <p>
-      This web app serves to help you plan your trip to Prince Edward Island.
+      <h4>Welcome to your personal travel companion!</h4>
+    </p>
 
-      Never been? No worries, this app will give you suggestions based on your chosen itinerary type.
+    <p>
+      Our itinerary planner is designed to make exploring new places easy, exciting, and stress-free. Whether you're planning a day trip, a weekend getaway, or a full vacation, our tool helps you discover hidden gems, must-see attractions, and local favorites — all customized to your interests.
+    </p>
+    <p>
+      We believe that every trip should feel unique. That's why we offer flexible itineraries you can personalize, swap activities you’re less excited about, and create an adventure that’s truly yours.
+    </p>
+    <p>
+      Simple, smart, and spontaneous — your next unforgettable journey starts here.
+    </p>
+    <p>
       Give it a try, its free to use.
     </p>
   </div>
@@ -14,8 +24,7 @@
 @media (min-width: 1024px) {
   .about {
     min-height: 100vh;
-    display: flex;
-    align-items: center;
+    display: block;
   }
 }
 </style>
